80166c9503a7b8e59400a3131057b0473b37a325
Hamalys <>
2023-08-22 11:02:57 +0000

    add some change on js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
-// Define your questions and initialize game state
+// Define  questions and initialize game state
 let questions = [
-    { category: 'Arithematic', points: 300, question: '8 multiplied by 6', answer: 42 },
-    { category: "Arithmetic", points: 300, question: "2 + 3", answer: 5 },
-
+    { category: 'Arithmetic', points: 100, question: 'What is 8 multiplied by 6?', answer: 48 },
+    { category: 'Arithmetic', points: 200, question: 'What is the result of 5 + 7?', answer: 12 },
+    { category: 'Arithmetic', points: 250, question: 'What is 10 divided by 2?', answer: 5 },
+    { category: 'Arithmetic', points: 300, question: 'What is 20 minus 7?', answer: 13 },
 ];
 
-let currentPlayer = 1; 
-let player1Score = 0; 
+let currentPlayer = 1;
+let player1Score = 0;
 let player2Score = 0;
 let timer;
 let timeLeft = 10; // 10 seconds per question
 
-// Function to display the game board
-
-
+// Display the game board
 function displayGameBoard() {
     let gameBoard = document.getElementById("game-board");
     // Clear any existing content inside the game board
@@ -21,7 +20,7 @@ function displayGameBoard() {
     // Loop through your questions to create category buttons and point values
     questions.forEach((questionObj) => {
         // Create a category button for each category
-        const categoryButton = document.createElement("button");
+        let categoryButton = document.createElement("button");
         categoryButton.textContent = questionObj.category;
         categoryButton.addEventListener("click", () => {
             // Handle category button click
@@ -34,6 +33,7 @@ function displayGameBoard() {
         for (let i = 0; i < 4; i++) {
             let pointValueButton = document.createElement("button");
             pointValueButton.textContent = `${i === 0 ? questionObj.points : questionObj.points - i * 50}`;
+            pointValueButton.dataset.points = i === 0 ? questionObj.points : questionObj.points - i * 50;
             pointValueButton.addEventListener("click", () => {
                 // Handle point value button click
                 // You can implement logic to display the question for this point value here
@@ -46,15 +46,22 @@ function displayGameBoard() {
     });
 }
 
-
-
-
-
-
-
 // Function to display a question
 function displayQuestion(questionObj) {
-    // Add code to display the question and answer choices
+    // Display the question and answer choices
+    document.getElementById("question1").textContent = `Question: ${questionObj.question}`;
+    let answerChoices = document.getElementById("answer-choices1");
+    let buttons = answerChoices.getElementsByTagName("button");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].textContent = `Option ${i + 1}: ${getRandomNumber()}`;
+        buttons[i].dataset.answer = buttons[i].textContent.split(":")[1].trim();
+        buttons[i].addEventListener("click", () => {
+            checkAnswer(questionObj.answer);
+        });
+    }
+
+    // Remove the selected point value button
+    document.querySelector(`button[data-points="${questionObj.points}"]`).remove();
 }
 
 // Function to generate a random incorrect answer
@@ -63,14 +70,14 @@ function getRandomNumber() {
 }
 
 // Function to check the answer
-function checkAnswer(correctAnswer, points) {
-    const selectedOption = parseInt(event.target.textContent.split(":")[1].trim());
+function checkAnswer(correctAnswer) {
+    let selectedOption = parseInt(event.target.dataset.answer);
     if (selectedOption === correctAnswer) {
         // Correct answer
         if (currentPlayer === 1) {
-            player1Score += points; // Update player-specific score
+            player1Score += parseInt(event.target.parentNode.parentNode.dataset.points); // Update player-specific score
         } else {
-            player2Score += points;
+            player2Score += parseInt(event.target.parentNode.parentNode.dataset.points);
         }
     }
 
@@ -84,9 +91,24 @@ function checkAnswer(correctAnswer, points) {
     startTimer();
 }
 
-// Function to update score display
-function updateScoreDisplay() {
-    // Add code to update the score display
+// To start the timer
+function startTimer() {
+    clearInterval(timer);
+    timeLeft = 10;
+    updateTimerDisplay();
+    timer = setInterval(() => {
+        timeLeft--;
+        updateTimerDisplay();
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            // Handle time's up logic here
+        }
+    }, 1000);
+}
+
+// Function to update timer display
+function updateTimerDisplay() {
+    document.getElementById("timer").textContent = `Time Left: ${timeLeft} seconds`;
 }
 
 // Initialize the game
