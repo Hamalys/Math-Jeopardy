5fd65396f52a108c870563d4d29e74046f8030f7
Hamalys <>
2023-08-28 10:25:59 +0000

    add change time displayed feedback element and restart element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//  questions and answers 
+// Questions and answers
 const questions = [
     { question: "What is 9 multiplied by 7?", answer: 63 },
     { question: "Calculate 12 divided by 4.", answer: 3 },
@@ -29,7 +29,7 @@ function displayQuestion(questionIndex) {
 
     startTimer();
 
-    // Handle correct/incorrect answers 
+    // Handle correct/incorrect answers
     const answerElement = document.createElement("div");
     answerElement.id = "answer-feedback";
     questionContainer.appendChild(answerElement);
@@ -44,6 +44,7 @@ function displayQuestion(questionIndex) {
         const userAnswer = parseInt(answerInput.value);
         if (!isNaN(userAnswer) && userAnswer === question.answer) {
             answerElement.textContent = "Correct!";
+            answerElement.classList.add("correct-feedback");
             if (currentPlayer === 1) {
                 player1Score++;
             } else {
@@ -51,12 +52,13 @@ function displayQuestion(questionIndex) {
             }
         } else {
             answerElement.textContent = "Incorrect!";
+            answerElement.classList.add("incorrect-feedback");
         }
         questionsAnswered++;
         if (questionsAnswered < questions.length) {
             setTimeout(() => displayQuestion(questionsAnswered), 2000);
         } else {
-            setTimeout(calculateWinner, 2000); 
+            setTimeout(calculateWinner, 2000);
         }
     });
 }
@@ -64,8 +66,9 @@ function displayQuestion(questionIndex) {
 // Function to start the timer
 function startTimer() {
     clearInterval(timer);
-    let timeLeft = 10; 
+    let timeLeft = 10;
     updateTimerDisplay();
+
     timer = setInterval(() => {
         timeLeft--;
         updateTimerDisplay();
@@ -74,16 +77,20 @@ function startTimer() {
             currentPlayer = currentPlayer === 1 ? 2 : 1;
             questionsAnswered++;
             if (questionsAnswered < questions.length) {
-                setTimeout(() => displayQuestion(questionsAnswered), 2000); 
+                setTimeout(() => displayQuestion(questionsAnswered), 2000);
             } else {
-                setTimeout(calculateWinner, 2000); 
+                setTimeout(calculateWinner, 2000);
             }
         }
     }, 1000);
 }
+
+// Function to update the timer display
 function updateTimerDisplay() {
     document.getElementById("timer").textContent = `Time Left: ${timeLeft} seconds`;
 }
+
+// Function to calculate the winner
 function calculateWinner() {
     let winnerMessage;
     if (player1Score > player2Score) {
@@ -93,10 +100,20 @@ function calculateWinner() {
     } else {
         winnerMessage = "It's a tie!";
     }
-    document.getElementById("game-container").innerHTML = `<h1>Game Over!</h1><p>${winnerMessage}</p>`;
-    document.getElementById("current-player").textContent = "";
+    document.getElementById("winner-message").textContent = winnerMessage;
     document.getElementById("player1-score").textContent = `Player 1: ${player1Score} points`;
     document.getElementById("player2-score").textContent = `Player 2: ${player2Score} points`;
     document.getElementById("winner-container").style.display = "block";
 }
+
+// Start the game by displaying the first question
 displayQuestion(0);
+
+// Restart button event listener
+document.getElementById("restart-button").addEventListener("click", () => {
+    player1Score = 0;
+    player2Score = 0;
+    currentPlayer = 1;
+    questionsAnswered = 0;
+    displayQuestion(0);
+});
